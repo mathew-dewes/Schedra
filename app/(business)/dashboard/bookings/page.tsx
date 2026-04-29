d8ea40a653daf6488d86a60ad009ca88743229c7
Mathew Dewes <mathewdewes01@gmail.com>
2026-04-30 09:32:05 +1200

    Initial data table added

--- app/(business)/dashboard/bookings/page.tsx
+++ app/(business)/dashboard/bookings/page.tsx
@@ -1,7 +1,44 @@
-export default function page(){
+import { BookingColumns, Payment } from "./_components/Bookingcolumn";
+import { BookingTable } from "./_components/BookingTable";
+
+
+    async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function page(){
+
+  const data = await getData()
     return (
-        <div>
-            <p>Booking page</p>
-        </div>
+       <div className="container mx-auto py-10">
+      <BookingTable columns={BookingColumns} data={data} />
+    </div>
     )
 }
