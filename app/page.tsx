452fb4f2ea403c39a7a95bc751e964675c723698
Mathew Dewes <mathewdewes01@gmail.com>
2026-05-21 11:37:19 +1200

    Vehicle and center CSV files added with database populated

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import Hero from "@/components/web/landing/Hero";
 import HowItWorks from "@/components/web/landing/HowItWorks";
 import Inspiration from "@/components/web/landing/Inspiration";
 
-export default function page(){
+
+export default async function page(){
   return (
     <div className="space-y-15 mb-40">
       <Hero/>
